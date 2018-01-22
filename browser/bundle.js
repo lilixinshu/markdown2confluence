8068dcb857e6952dd9a3e008fefc2ba0155837c9
shanfl <shanfl@jinxiang.com>
2018-01-22 17:44:38 +0800

    modify

--- browser/bundle.js
+++ browser/bundle.js
@@ -350,24 +350,24 @@ function init() {
 /* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var marked = __webpack_require__(6)
-var _ = __webpack_require__(7)
-var qs = __webpack_require__(16)
-var inlineLexer = marked.inlineLexer
+var marked = __webpack_require__(6);
+var _ = __webpack_require__(7);
+var qs = __webpack_require__(16);
+var inlineLexer = marked.inlineLexer;
 
-module.exports = exports = markdown2confluence
+module.exports = exports = markdown2confluence;
 
 // https://roundcorner.atlassian.net/secure/WikiRendererHelpAction.jspa?section=all
 // https://confluence.atlassian.com/display/DOC/Confluence+Wiki+Markup
 // http://blogs.atlassian.com/2011/11/why-we-removed-wiki-markup-editor-in-confluence-4/
 
-var MAX_CODE_LINE = 20
+var MAX_CODE_LINE = 20;
 
-function Renderer() { }
+function Renderer() { };
 
-var rawRenderer = marked.Renderer
+var rawRenderer = marked.Renderer;
 
-var langArr = 'actionscript3 bash csharp coldfusion cpp css delphi diff erlang groovy java javafx javascript perl php none powershell python ruby scala sql vb html/xml'.split(/\s+/)
+var langArr = 'actionscript3 bash csharp coldfusion cpp css delphi diff erlang groovy java javafx javascript perl php none powershell python ruby scala sql vb html/xml'.split(/\s+/);
 var langMap = {
 	'actionscript3': 'actionscript3',
 	'applescript': 'applescript',
@@ -402,104 +402,114 @@ var langMap = {
 	'shell': 'bash',
 	'html': 'xml',
 	'xml': 'xml'
-}
+};
 for (var i = 0, x; x = langArr[i++];) {
-	langMap[x] = x
+	langMap[x] = x;
+}
+
+var escape = function(text){
+	text = text.replace(/\{\{/g, '__markdown2confluence_brackets_front__').replace(/\{/g, '\\{').replace(/__markdown2confluence_brackets_front__/g, '{{');
+	text = text.replace(/\}\}/g, '__markdown2confluence_brackets_end__').replace(/\}/g, '\\}').replace(/__markdown2confluence_brackets_end__/g, '}}');
+	text = text.replace(/\[/g,"\\[").replace(/\]/g,"\\]");
+	text = text.replace(/\\</g, '<').replace(/\\\&lt;/g, '<').replace(/\\>/g,'>').replace(/\\\&gt;/g,'>');
+	text = text.replace(/__markdown2confluence_square_front__/g, '[').replace(/__markdown2confluence_square_end__/g, ']');
+	return text;
 }
 
 _.extend(Renderer.prototype, rawRenderer.prototype, {
 	paragraph: function (text) {
-		text = text.replace(/\{\{/g, '__markdown2confluence_brackets_front__').replace(/\{/g, '\\{').replace(/__markdown2confluence_brackets_front__/g, '{{');
-		text = text.replace(/\}\}/g, '__markdown2confluence_brackets_end__').replace(/\}/g, '\\}').replace(/__markdown2confluence_brackets_end__/g, '}}');
-		return text + '\n\n'
+		// text = text.replace(/\{\{/g, '__markdown2confluence_brackets_front__').replace(/\{/g, '\\{').replace(/__markdown2confluence_brackets_front__/g, '{{');
+		// text = text.replace(/\}\}/g, '__markdown2confluence_brackets_end__').replace(/\}/g, '\\}').replace(/__markdown2confluence_brackets_end__/g, '}}');
+		// text = text.replace(/\[/g,"\\[").replace(/\]/g,"\\]");
+		return escape(text) + '\n\n';
 	}
 	, html: function (html) {
-		return html
+		return html;
 	}
 	, heading: function (text, level, raw) {
-		return 'h' + level + '. ' + text + '\n\n'
+		return 'h' + level + '. ' + text + '\n\n';
 	}
 	, strong: function (text) {
-		return '*' + text + '*'
+		return '*' + text + '*';
 	}
 	, em: function (text) {
-		return '_' + text + '_'
+		return '_' + text + '_';
 	}
 	, del: function (text) {
-		return '-' + text + '-'
+		return '-' + text + '-';
 	}
 	, codespan: function (text) {
-		return '{{' + text + '}}'
+		return '{{' + text + '}}';
 	}
 	, blockquote: function (quote) {
-		return '{quote}' + quote + '{quote}'
+		return '{quote}' + quote + '{quote}';
 	}
 	, br: function () {
-		return '\n'
+		return '\n';
 	}
 	, hr: function () {
-		return '----'
+		return '----';
 	}
 	, link: function (href, title, text) {
-		var arr = [href]
+		var arr = [href];
 		if (text) {
-			arr.unshift(text)
+			arr.unshift(text);
 		}
-		return '[' + arr.join('|') + ']'
+		return '__markdown2confluence_square_front__' + arr.join('|') + '__markdown2confluence_square_end__';
 	}
 	, list: function (body, ordered) {
 		var arr = _.filter(_.trim(body).split('\n'), function (line) {
-			return line
+			return line;
 		})
-		var type = ordered ? '#' : '*'
+		var type = ordered ? '#' : '*';
 		return _.map(arr, function (line) {
-			return type + ' ' + line
-		}).join('\n') + '\n\n'
+			return type + ' ' + escape(line);
+		}).join('\n') + '\n\n';
 
 	}
 	, listitem: function (body, ordered) {
-		return body + '\n'
+		return body + '\n';
 	}
 	, image: function (href, title, text) {
-		return '!' + href + '!'
+		return '!' + href + '!';
 	}
 	, table: function (header, body) {
-		return header + body + '\n'
+		return header + body + '\n';
 	}
 	, tablerow: function (content, flags) {
-		return content + '\n'
+		return content + '\n';
 	}
 	, tablecell: function (content, flags) {
-		var type = flags.header ? '||' : '|'
-		return type + content
+		var type = flags.header ? '||' : '|';
+		return type + content;
 	}
 	, code: function (code, lang) {
 		// {code:language=java|borderStyle=solid|theme=RDark|linenumbers=true|collapse=true}
 		if (lang) {
-			lang = lang.toLowerCase()
+			lang = lang.toLowerCase();
 		}
-		lang = langMap[lang] || 'none'
+		lang = langMap[lang] || 'none';
 		var param = {
 			language: lang,
 			borderStyle: 'solid',
 			theme: 'RDark', // dark is good
 			linenumbers: true,
 			collapse: false
-		}
-		var lineCount = _.split(code, '\n').length
+		};
+		var lineCount = _.split(code, '\n').length;
 		if (lineCount > MAX_CODE_LINE) {
 			// code is too long
 			//param.collapse = true
 		}
-		param = qs.stringify(param, '|', '=')
-		return '{code:' + param + '}\n' + code + '\n{code}\n\n'
+		param = qs.stringify(param, '|', '=');
+		return '{code:' + param + '}\n' + code + '\n{code}\n\n';
 	}
 })
 
-var renderer = new Renderer()
+var renderer = new Renderer();
 
 function markdown2confluence(markdown) {
-	return marked(markdown, { renderer: renderer })
+	return marked(markdown, { renderer: renderer });
 }
 
 
